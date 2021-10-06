937853815fd37770a469958546bdcb15e938f025
Destroyer Rules <kuskov779@gmail.com>
2021-10-06 09:17:55 +0300

    Work!

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-from manage import Server
-from config import vk_api_token
+# from manage import Server
+# from config import vk_api_token
 
 
-server = Server(vk_api_token, 202553267, "Conspirology_bot")
-server.start()
+# server = Server(vk_api_token, 202553267, "Conspirology_bot")
+# server.start()
+
+import random
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import config
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(0, 2048)})
+
+
+# API-ключ созданный ранее
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=config.vk_api_token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+print("Бот запущен")
+# Основной цикл
+for event in longpoll.listen():
+
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
+
+            # Сообщение от пользователя
+            request = event.text
+
+            # Каменная логика ответа
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
